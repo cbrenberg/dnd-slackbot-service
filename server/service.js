4a545d419b8d4bae9c03534532d9fab2d2e011f8
Christopher Brenberg <cbrenberg@gmail.com>
2019-01-28 16:40:41 -0600

    refactor cached spells, improve error handling, reformat response string

--- server/service.js
+++ server/service.js
@@ -7,53 +7,59 @@ const { request, GraphQLClient } = require('graphql-request');
 
 const client = new GraphQLClient(process.env.GRAPHQL_ENDPOINT);
 
-const spellListCache = async () => {
-  await client.request(`
-{
-  spells {
-    index
-    name
-  }
-}
-`)
-    .then(res => {
-      console.log(res);
-      return res;
-    })
-    .catch(err => console.log(err));
-};
+//initialize in-memory cache variable of all spell indices and names on startup
+let spellListCache;
 
-//http://dataservice.accuweather.com/locations/v1/cities/search?apiKey=${process.env.ACCUWEATHER_API_KEY}q=${req.query.location}
+//graphQL request for cache
+client.request(`{ spells 
+                  {
+                    index
+                    name
+                  }
+                }`)
+  .then(res => {
+    // console.log('back with spells for cache:', res);
+    spellListCache = res;
+  })
+  .catch(err => console.log('error setting cached spell list:', err));
+
+//handle spell requests from host app
 service.get('/service/spell/:spellName', async (req, res, next) => {
 
-  console.log(spellListCache.spells);
   const spellName = req.params.spellName;
-  console.log('incoming spell name:', spellName);
+  let spellMatch;
+  //find matching spell from cache by name
+  try {
+    spellMatch = spellListCache.spells.filter(spell => spell.name.includes(spellName));
+  } catch (err) {
+    console.log('Error retrieving spell from cache', err);
+  }
 
-  const spellMatch = spellListCache.spells.filter(spell => spell.name.includes(spellName));
-  console.log('Found matching spell name!', spellMatch)
-
+  //request spell details from graphQL server by spell index
   client.request(`{
   spell (index: ${spellMatch[0].index}) {
       name
       description
     }
   }`)
-    .then(data => console.log('spell details:', data))
-    .catch(err => console.log(err));
-  // try {
-  //   let locationResponse = await axios.get(`http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${process.env.ACCUWEATHER_API_KEY}&q=${req.query.lat},${req.query.lng}`);
-  //   console.log("location response data:", locationResponse.data);
-  //   const locationCode = locationResponse && locationResponse.data && locationResponse.data.Key ? locationResponse.data.Key : null;
-  //   console.log("location Code:", locationCode);
-  //   let weatherResponse = await axios.get(`http://dataservice.accuweather.com/currentconditions/v1/${locationCode}?apikey=${process.env.ACCUWEATHER_API_KEY}`);
-  //   console.log("weather response data:", weatherResponse.data);
-  //   const localWeatherString = `${weatherResponse.data[0].WeatherText} and ${weatherResponse.data[0].Temperature.Imperial.Value} degrees`;
-  //   console.log("local weather string response:", localWeatherString);
-  //   res.send(localWeatherString.toLowerCase());
-  // } catch (error) {
-  //   console.log(error.data);
-  //   res.status(500).send(error.data);
+    .then(response => {
+      //send spell description back to host app
+      let stringResponse = response.spell.description;
+      res.status(200).send(stringResponse);
+    })
+    .catch(err => {
+      console.log('graphQL request error:', err);
+      res.sendStatus(500);
+    });
+
+  // **EXAMPLE SPELL DETAIL RESPONSE**
+
+  //{ spell:
+  //   {
+  //     name: 'Mage Hand',
+  //       description:
+  //     'A spectral, floating hand appears at a point you choose within range. The hand lasts for the duration or until you dismiss it as an action. The hand vanishes if it is ever more than 30 feet away from you or if you cast this spell again.'
+  //   }
   // }
 
 });
