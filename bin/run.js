55124912ed487eca21e9647a6dca2e2d5f800913
Christopher Brenberg <cbrenberg@gmail.com>
2019-01-28 16:39:50 -0600

    move host url to env var

--- bin/run.js
+++ bin/run.js
@@ -10,8 +10,8 @@ const service = require('../server/service');
 const server = service.listen(process.env.PORT, () => {
   console.log(`IRIS-dnd-spells is listening on port ${process.env.PORT} in ${service.get('env')} mode`);
 
-  //announce itself to host application's service registry
-  const announce = () => axios.put(`http://localhost:3000/service/spell/${server.address().port}`)
+  //announce service to host application's service registry
+  const announce = () => axios.put(`${process.env.HOST_BASE_URL}/service/spell/${server.address().port}`)
     .then(response => {
       console.log(response.data);
     })
