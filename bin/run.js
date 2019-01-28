67f4b48ac17e869eb609aa64ef888f7dd5b7eb32
Christopher Brenberg <cbrenberg@gmail.com>
2019-01-28 16:50:36 -0600

    change name for service registry

--- bin/run.js
+++ bin/run.js
@@ -8,14 +8,14 @@ require('dotenv').config();
 const service = require('../server/service');
 
 const server = service.listen(process.env.PORT, () => {
-  console.log(`IRIS-dnd-spells is listening on port ${process.env.PORT} in ${service.get('env')} mode`);
+  console.log(`dnd-slackbot-service is listening on port ${process.env.PORT} in ${service.get('env')} mode`);
 
   //announce service to host application's service registry
   const announce = () => axios.put(`${process.env.HOST_BASE_URL}/service/spell/${server.address().port}`)
     .then(response => {
       console.log(response.data);
     })
-    .catch(err => console.log('There was an error connecting to Iris', err))
+    .catch(err => console.log('There was an error connecting to service host', err))
 
   announce();
   //re-announce every 15 seconds
