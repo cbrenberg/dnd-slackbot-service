10eb230c858b465b20255cfa804c492e1ea617d2
Christopher Brenberg <cbrenberg@gmail.com>
2019-02-03 16:24:56 -0600

    encode self URL in service announcement parameter

--- bin/run.js
+++ bin/run.js
@@ -7,13 +7,14 @@ require('dotenv').config();
 //require modules
 const service = require('../server/service');
 
-const server = service.listen(process.env.PORT, () => {
+service.listen(process.env.PORT, () => {
   console.log(`dnd-slackbot-service is listening on port ${process.env.PORT} in ${service.get('env')} mode`);
 
   //announce service to host application's service registry
-  const announce = () => axios.put(`${process.env.HOST_BASE_URL}/service/spell/${process.env.SELF_URL}`)
+  const address = encodeURIComponent(process.env.SELF_URL);
+  const announce = () => axios.put(`${process.env.HOST_BASE_URL}/service/spell/${address}`)
     .then(response => {
-      console.log(`announced service to ${process.env.HOST_BASE_URL}/service/spell/${process.env.SELF_URL}`);
+      console.log(`announced service to ${process.env.HOST_BASE_URL}/service/spell/${address}`);
       console.log("Connected to service host", response.data);
     })
     .catch(err => console.log('Error connecting to service host'))
